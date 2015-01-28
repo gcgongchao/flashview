d6f3982fd33a8c8a0ad07f64e79c2e945aec335c
gcgongchao <gc_gongchao@163.com>
2015-01-28 21:16:57 +0800

    加入自定义效果常量

---
+++ src/com/gc/flashview/constants/EffectConstants.java
@@ -0,0 +1,18 @@
+package com.gc.flashview.constants;
+/**
+ * 
+ * @author Android将军
+ * 声明效果常量
+ *
+ */
+public class EffectConstants {
+
+	public static final int ACCORDTION_EFFECT=0;
+	public static final int CUBE_EFFECT=1;
+	public static final int DEFAULT_EFFECT=2;
+	public static final int DEPTH_PAGE_EFFECT=3;
+	public static final int IN_RIGHT_DOWN_EFFECT=4;
+	public static final int IN_RIGHT_UP_EFFECT=5;
+	public static final int ROTATE_EFFECT=6;
+	public static final int ZOOM_OUT_PAGE_EFFECT=7;
+}
